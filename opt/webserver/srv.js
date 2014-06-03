19b974754e4750a2446a6b3e7aa7c3297acbee6f
sebseb7 <github@seb.exse.net>
2014-06-04 01:01:32 +0200

    player management

--- opt/webserver/srv.js
+++ opt/webserver/srv.js
@@ -1,10 +1,11 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 var fs = require('fs');
 
 var httpServer = require('http').createServer(handler);
 
 var io = require('socket.io').listen(httpServer);
 
+//httpServer.listen(8000,'127.0.0.1');
 httpServer.listen(80,'10.10.11.10');
 
 //io.enable('browser client minification');
@@ -37,9 +38,54 @@ function handler (req,res) {
 	});
 } 
 
+
+var players = [];
+
+
+players[0]=0;
+players[1]=0;
+players[2]=0;
+
+
 io.sockets.on('connection', function (socket) {
 	
 	console.log('socket.io conn');
+	
+
+	
+	socket.on('disconnect', function () {
+
+		if(socket.playernumber != 0)
+		{
+			players[socket.playernumber-1]=0;
+		}
+
+	});
+
+
+	var myPlayer=0;
+	if(players[0]==0)
+	{
+		myPlayer=1;
+	} else
+	if(players[1]==0)
+	{
+		myPlayer=2;
+	} else
+	if(players[2]==0)
+	{
+		myPlayer=3;
+	}
+
+	if(myPlayer != 0)
+	{
+		players[myPlayer-1]=1;
+	}
+
+
+	socket.playernumber = myPlayer;
+
+	socket.emit('player',myPlayer);
 });
 
 
